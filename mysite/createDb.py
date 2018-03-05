81d7c7060d445aaf9483a5eda919f863b6aed63e
Evan <ejohnson16@cougars.ccis.edu>
2018-03-05 22:45:03 +0000

    stuff

--- mysite/createDb.py
+++ mysite/createDb.py
@@ -1,19 +1,19 @@
-import MySQLdb
+# import MySQLdb
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="john",         # your username
-                     passwd="megajonhy",  # your password
-                     db="jonhydb")        # name of the data base
+# db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+#                      user="john",         # your username
+#                      passwd="megajonhy",  # your password
+#                      db="jonhydb")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
+# # you must create a Cursor object. It will let
+# #  you execute all the queries you need
+# cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+# # Use all the SQL you like
+# cur.execute("SELECT * FROM YOUR_TABLE_NAME")
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print(row[0])
+# # print all the first cell of all the rows
+# for row in cur.fetchall():
+#     print(row[0])
 
-db.close()
+# db.close()
--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -2,27 +2,27 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
-import storeSignup.py
+# import storeSignup.py
 
-import MySQLdb
+# import MySQLdb
 
-db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="john",         # your username
-                     passwd="megajonhy",  # your password
-                     db="jonhydb")        # name of the data base
+# db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+#                      user="john",         # your username
+#                      passwd="megajonhy",  # your password
+#                      db="jonhydb")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
+# # you must create a Cursor object. It will let
+# #  you execute all the queries you need
+# cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+# # Use all the SQL you like
+# cur.execute("SELECT * FROM YOUR_TABLE_NAME")
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print row[0]
+# # print all the first cell of all the rows
+# for row in cur.fetchall():
+#     print row[0]
 
-db.close()
+# db.close()
 
 app = Flask(__name__)
 
